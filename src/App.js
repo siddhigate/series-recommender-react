8c51c6e0ec0e2d31421bc56f7a75a84dc10131a4
siddhigate <80971056+siddhigate@users.noreply.github.com>
2021-09-17 00:56:47 +0530

    feat: added description to series

--- src/App.js
+++ src/App.js
@@ -1,34 +1,113 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-//data
 const seriesData = {
   fiction: [
-    { name: "Stranger Things", rating: "imdb 8.8/10" },
-    { name: "Black Mirror", rating: "imdb 8.8/10" },
-    { name: "The Walking Dead", rating: "imdb 7.6/10" },
-    { name: "The 100", rating: "imdb 7.6/10" }
+    {
+      name: "Stranger Things",
+      description:
+        "Stranger Things is an American science fiction horror drama television series",
+      rating: "imdb 8.8/10"
+    },
+    {
+      name: "Black Mirror",
+      description:
+        "This sci-fi anthology series explores a twisted, high-tech near-future ",
+      rating: "imdb 8.8/10"
+    },
+    {
+      name: "The Walking Dead",
+      description:
+        "The Walking Dead is an American post-apocalyptic horror television series based on the comic book series",
+      rating: "imdb 7.6/10"
+    },
+    {
+      name: "The 100",
+      description:
+        "Desperately looking for a solution, the ark's leaders send 100 juvenile prisoners back to the planet to test its habitability.",
+      rating: "imdb 7.6/10"
+    }
   ],
 
   sitcom: [
-    { name: "The Office", rating: "imdb 8.9/10" },
-    { name: "Brooklyn 99", rating: "imdb 8.4/10" },
-    { name: "Friends", rating: "imdb 8.1/10" },
-    { name: "The Big Bang Theory", rating: "imdb 8.1/10" }
+    {
+      name: "The Office",
+      description:
+        "The Office is an American mockumentary sitcom television series that depicts the everyday work lives of office employees",
+      rating: "imdb 8.9/10"
+    },
+    {
+      name: "Brooklyn 99",
+      description:
+        "Brooklyn Nine-Nine is an American police procedural comedy television series",
+      rating: "imdb 8.4/10"
+    },
+    {
+      name: "Friends",
+      description:
+        "Friends is an American situation comedy about six 20-30s-year old friends living in the New York City",
+      rating: "imdb 8.1/10"
+    },
+    {
+      name: "The Big Bang Theory",
+      description:
+        "The lives of four socially awkward friends, Leonard, Sheldon, Howard and Raj, take a wild turn when they meet the beautiful and free-spirited Penny.",
+      rating: "imdb 8.1/10"
+    }
   ],
 
   drama: [
-    { name: "Pretty Little Liars", rating: "imdb 7.4/10" },
-    { name: "The Vampire Diaries", rating: "imdb 7.7/10" },
-    { name: "Riverdale", rating: "imdb 6.9/10" },
-    { name: "Gossip Girl", rating: "imdb 7.4/10" }
+    {
+      name: "Pretty Little Liars",
+      description:
+        "The series follows the lives of four teenage girls - Aria, Spencer, Hanna and Emily; four estranged friends whose darkest secrets are about to unravel.",
+      rating: "imdb 7.4/10"
+    },
+    {
+      name: "The Vampire Diaries",
+      description:
+        "The Vampire Diaries is an American supernatural teen drama television series developed by Kevin Williamson and Julie Plec",
+      rating: "imdb 7.7/10"
+    },
+    {
+      name: "Riverdale",
+      description:
+        "Riverdale is an American teen drama television series based on the characters of Archie Comics.",
+      rating: "imdb 6.9/10"
+    },
+    {
+      name: "Gossip Girl",
+      description:
+        "Gossip Girl is an American teen drama television series based on the novel series of the same name written by Cecily von Ziegesar.",
+      rating: "imdb 7.4/10"
+    }
   ],
 
   horror: [
-    { name: "Haunting of Hill House", rating: "imdb 8.6/10" },
-    { name: "Haunting of Bly Manor", rating: "imdb 7.4/10" },
-    { name: "Chilling Adventures of Sabrina", rating: "imdb 7.5/10" },
-    { name: "Locke and Key", rating: "imdb 7.4/10" }
+    {
+      name: "Haunting of Hill House",
+      description:
+        "The Haunting of Hill House is an American supernatural horror drama television series created and directed by Mike Flanagan",
+      rating: "imdb 8.6/10"
+    },
+    {
+      name: "Haunting of Bly Manor",
+      description:
+        "The Haunting of Bly Manor is an American drama-gothic romance streaming television series created by Mike Flanagan",
+      rating: "imdb 7.4/10"
+    },
+    {
+      name: "Chilling Adventures of Sabrina",
+      description:
+        "Chilling Adventures of Sabrina is an American supernatural horror streaming television series developed by Roberto Aguirre-Sacasa for Netflix",
+      rating: "imdb 7.5/10"
+    },
+    {
+      name: "Locke and Key",
+      description:
+        "Locke & Key is a 2020 American supernatural horror drama streaming television series developed by Carlton Cuse, Meredith Averill, and Aron Eli Coleite",
+      rating: "imdb 7.4/10"
+    }
   ]
 };
 
@@ -62,6 +141,12 @@ var largeFont = {
 
 var smallFont = {
   fontSize: "smaller"
+};
+
+var grayFont = {
+  fontSize: "smaller",
+  color: "gray",
+  margin: "0.25rem 0"
 };
 
 var navBrand = {
@@ -112,6 +197,7 @@ export default function App() {
               <li style={listStyle}>
                 <div style={largeFont}>
                   {type.name}
+                  <div style={grayFont}>{type.description}</div>
                   <div style={smallFont}>{type.rating}</div>
                 </div>
               </li>
